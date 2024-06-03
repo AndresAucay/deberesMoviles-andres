43969515d0debc6c4c81734c20690976ba0e89b0
Andres <maaucay@sudamericano.edu.ec>
2024-06-03 08:02:25 -0500

    ultima correccion

--- 2.1 React Router/TRABAJO EN CLASE/NEW-APP/new-app/src/pages/products/ProductCreate.tsx
+++ 2.1 React Router/TRABAJO EN CLASE/NEW-APP/new-app/src/pages/products/ProductCreate.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+    import { Create, useForm } from "@refinedev/antd";
+    import { Form, Input } from "antd";
 
-const ProductCreate: React.FC = () => {
-    return <div>Create Product</div>;
-};
+    export const ProductCreate = () => {
+    const { formProps, saveButtonProps } = useForm({});
 
-export default ProductCreate;
+    return (
+        <Create saveButtonProps={saveButtonProps}>
+        <Form {...formProps} layout="vertical">
+            <Form.Item
+            label={"Title"}
+            name={["title"]}
+            rules={[
+                {
+                required: true,
+                },
+            ]}
+            >
+            <Input />
+            </Form.Item>
+        </Form>
+        </Create>
+    );
+    };
